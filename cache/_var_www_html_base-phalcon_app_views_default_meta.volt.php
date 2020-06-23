da5ecffca47be4d407cf862faae1558a3548f7c9
gz <germano@suamusica.com.br>
2020-06-22 21:58:09 -0300

    removing cache

--- cache/_var_www_html_base-phalcon_app_views_default_meta.volt.php
+++
@@ -1,5 +0,0 @@
-<meta name="description" content="<?= $this->tag->getvalue('descricao') ?>">
-<meta property="og:title" content="<?= $this->tag->getvalue('fbtitle') ?>">
-<meta property="og:image" content="<?= $this->tag->getvalue('fbimage') ?>">
-<meta property="og:description" content="<?= $this->tag->getvalue('descricao') ?>">
-<meta property="og:url" content="<?= $this->tag->getvalue('url') ?>">
